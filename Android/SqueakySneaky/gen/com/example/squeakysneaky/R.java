95fa5301a566b377fc04923c816ba8c21db8c33e
David <davidphillips92@gmail.com>
2014-09-13 12:56:10 +1000

    GetIP

--- Android/SqueakySneaky/gen/com/example/squeakysneaky/R.java
+++ Android/SqueakySneaky/gen/com/example/squeakysneaky/R.java
@@ -31,8 +31,9 @@ public final class R {
         public static final int logo=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080003;
         public static final int homeButton1=0x7f080001;
+        public static final int homeButton2=0x7f080002;
         public static final int homeImageView=0x7f080000;
     }
     public static final class layout {
--- Android/SqueakySneaky/src/com/example/squeakysneaky/HomeActivity.java
+++ Android/SqueakySneaky/src/com/example/squeakysneaky/HomeActivity.java
@@ -1,8 +1,17 @@
 package com.example.squeakysneaky;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.text.format.Formatter;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,8 +24,8 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         
-        Button button = (Button) findViewById(R.id.homeButton1);
-        button.setOnClickListener(new OnClickListener() {
+        Button getStartedButton = (Button) findViewById(R.id.homeButton1);
+        getStartedButton.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
@@ -24,8 +33,27 @@ public class HomeActivity extends Activity {
 				startActivity(intent);
 			}
 		});
+        
+        Button ipButton = (Button) findViewById(R.id.homeButton2);
+        ipButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				AlertDialog.Builder alert = new AlertDialog.Builder(HomeActivity.this);
+				alert.setTitle("My IP");
+				alert.setMessage(getIP());
+				alert.setNegativeButton("OK", new DialogInterface.OnClickListener() {
+					@Override
+					public void onClick(DialogInterface dialog, int arg1) {
+						dialog.cancel();
+					}
+				});
+				
+				alert.show();
+			}
+		});
+        
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,5 +61,16 @@ public class HomeActivity extends Activity {
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
+    
+    private String getIP(){
+    	String ipAddress = "null";
+        
+    	WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
+    	ipAddress = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        
+		return ipAddress;
+    }
+    
+    
     
 }
